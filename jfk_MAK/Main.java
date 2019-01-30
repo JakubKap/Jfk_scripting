4ad5536d57911dafc0cf8be4bb3b75774364853d
Mikołaj Bednarek <egosummiki@gmail.com>
2019-01-30 21:55:09 +0100

    Added JythonEngine. Added ability to set writers to stdout and stderr.

--- jfk_MAK/Main.java
+++ jfk_MAK/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import jfk_MAK.Model.Engine.Nashorn;
 import jfk_MAK.Model.Entity;
 import jfk_MAK.Model.CsvFile;
 
@@ -24,20 +23,6 @@ public class Main extends Application {
         System.out.println("Jakub K");
         CsvFile csvFile = new CsvFile();
         List<Entity> entities = csvFile.readFromCsvFile("clients.csv");
-
-        Nashorn nashorn = new Nashorn(entities);
-
-        nashorn.run("for each(e in entities) {\n" +
-                "\tprint(e.name);\n" +
-                "}"
-        );
-        nashorn.run("entities[0].name = 'Marek';");
-        nashorn.run("for each(e in entities) {\n" +
-                "\tprint(e.name);\n" +
-                "}"
-        );
-
-        System.out.println(nashorn.run("dsfdf =sdf + sdfsdf ff"));
     }
 
 
--- jfk_MAK/Model/Engine/Nashorn.java
+++ jfk_MAK/Model/Engine/Nashorn.java
@@ -5,16 +5,15 @@ import jfk_MAK.Model.Entity;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.Writer;
 import java.util.List;
 
 public class Nashorn {
 
-    private List<Entity> entities;
     private ScriptEngine engine;
 
     public Nashorn(List<Entity> entities) {
 
-        this.entities = entities;
         engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.put("entities", entities);
     }
@@ -28,5 +27,13 @@ public class Nashorn {
         }
 
         return null;
+    }
+
+    public void setStdout(Writer writer) {
+        engine.getContext().setWriter(writer);
+    }
+
+    public void setStderr(Writer writer) {
+        engine.getContext().setErrorWriter(writer);
     }
 }
