c6ab2bff8b8ff1b4eee12c9d4ae1f2303e24f4f4
Mikołaj Bednarek <egosummiki@gmail.com>
2019-01-30 21:07:18 +0100

    Added nashorn engine.

--- jfk_MAK/Main.java
+++ jfk_MAK/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jfk_MAK.Model.Engine.Nashorn;
 import jfk_MAK.Model.Entity;
 import jfk_MAK.Model.CsvFile;
 
@@ -14,18 +15,29 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/MainView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/mainView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
 
         System.out.println("Jakub K");
         CsvFile csvFile = new CsvFile();
         List<Entity> entities = csvFile.readFromCsvFile("clients.csv");
 
-        for(Entity entity : entities){
-            entity.toString();
-        }
+        Nashorn nashorn = new Nashorn(entities);
+
+        nashorn.run("for each(e in entities) {\n" +
+                "\tprint(e.name);\n" +
+                "}"
+        );
+        nashorn.run("entities[0].name = 'Marek';");
+        nashorn.run("for each(e in entities) {\n" +
+                "\tprint(e.name);\n" +
+                "}"
+        );
+
+        System.out.println(nashorn.run("dsfdf =sdf + sdfsdf ff"));
     }
 
 
