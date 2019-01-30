dcb87eb38c062a6fded4f71bc363d2e6cf54c6c7
Mikołaj Bednarek <egosummiki@gmail.com>
2019-01-30 22:36:10 +0100

    Added run buttons in controllers.

--- jfk_MAK/Controller/JytonTabController.java
+++ jfk_MAK/Controller/JytonTabController.java
@@ -1,11 +1,28 @@
 package jfk_MAK.Controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import jfk_MAK.Model.Engine.JythonEngine;
 
 public class JytonTabController {
 
     @FXML private TextArea inputArea;
 
     @FXML private TextArea outputArea;
+
+    @FXML private TextArea errOutputArea;
+
+    @FXML private Button runButton;
+
+    JythonEngine jythonEngine;
+
+    public JytonTabController() {
+
+        runButton.setOnMouseClicked(mouseEvent -> {
+
+        });
+    }
 }
--- jfk_MAK/Controller/NashornTabController.java
+++ jfk_MAK/Controller/NashornTabController.java
@@ -1,6 +1,7 @@
 package jfk_MAK.Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
 public class NashornTabController {
@@ -8,4 +9,9 @@ public class NashornTabController {
     @FXML private TextArea inputArea;
 
     @FXML private TextArea outputArea;
+
+    @FXML private TextArea errOutputArea;
+
+    @FXML private Button runButton;
+
 }
