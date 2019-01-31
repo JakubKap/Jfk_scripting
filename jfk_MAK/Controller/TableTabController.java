e479e2d237aab4176cb676de0e240ea62b54efbd
Mikołaj Bednarek <egosummiki@gmail.com>
2019-01-31 22:35:40 +0100

    Changed awt file dialog to javafx file chooser.

--- jfk_MAK/Controller/TableTabController.java
+++ jfk_MAK/Controller/TableTabController.java
@@ -1,27 +1,22 @@
 package jfk_MAK.Controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-import java.awt.*;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
 import jfk_MAK.Model.CsvFile;
 import jfk_MAK.Model.Entity;
+
+import javafx.scene.Node;
 
 public class TableTabController implements Initializable{
 
@@ -38,12 +33,11 @@ public class TableTabController implements Initializable{
     @FXML private Button deleteButton;
 
     private void loadCsv(ActionEvent event){
-        FileDialog dialog = new FileDialog((Frame)null, "Select a File to Open");
-        dialog.setMode(FileDialog.LOAD);
-        dialog.setVisible(true);
-        String path = dialog.getDirectory() + dialog.getFile();
-        CsvFile.getInstance().load(path);
-        dialog.dispose();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a file to open");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV", "*.csv"));
+        File file = fileChooser.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
+        CsvFile.getInstance().load(file.getPath());
 
         entityTable.getItems().clear();
         entityTable.setItems(CsvFile.getInstance().entities);
